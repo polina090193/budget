86cc127de28f9a6fe3a515ea510d7e76cf3a9107
Polina Guseva <polina_0901@mail.ru>
2023-10-17 16:12:44 +0200

    First draft pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../page.module.css'
+
+export default function Dashboard() {
+  return (
+    <main className={styles.main}>
+      <p>
+        Dashboard
+      </p>
+    </main>
+  )
+}
