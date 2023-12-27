0d2b87824c9b0278651b00a16cc80272abd7ccf9
Polina Guseva <polina_0901@mail.ru>
2023-12-27 11:28:43 +0100

    Fix date sent to DB

--- utils/adapters/dateSQLadapter.ts
+++ utils/adapters/dateSQLadapter.ts
@@ -1,1 +1,5 @@
-export const dateSQLadapter = (date: Date) => date.toISOString().slice(0, 19).replace('T', ' ');
+export const dateSQLadapter = (date: Date) => {
+  const utcDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
+
+  return utcDate.toISOString().slice(0, 19).replace('T', ' ');
+}
